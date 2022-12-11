c3bebd9f7dece10c487a57c4ef126ccddd98b738
Neel Adwani <nprosofficial@gmail.com>
2022-12-11 07:06:19 +0000

    Integrated with AssemblyAI API

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 from flask import Flask, request
+import os
+import utils
+import requests
+from replit import db
 
 app = Flask('app')
+db['transcription_url'] = ""
 
+def get_transcripts():
+  api_key = os.getenv("AAI_API_KEY")
+  header = {
+    'authorization': api_key,
+    'content-type': 'application/json'
+  }
+  audio_file = "alpha.mp3"
+  upload_url = utils.upload_file(audio_file, header)
+  transcript_response = utils.request_transcript(upload_url, header)
+  print(transcript_response)
+  polling_endpoint = utils.make_polling_endpoint(transcript_response)
+  db['transcription_url'] = polling_endpoint
+  utils.wait_for_completion(polling_endpoint, header)
+  paragraphs = utils.get_paragraphs(polling_endpoint, header)
+  with open('transcript.txt', 'w') as f:
+    for para in paragraphs:
+      print(para['text'] + '\n')
+      f.write(para['text'] + '\n')
+  return paragraphs
 
 @app.route('/', methods=["POST"])
 def hello_world():
@@ -9,9 +33,22 @@ def hello_world():
     file = request.files['file']
     if file:
       file.save("alpha.mp3")
-      return "file is here"
+      paragraphs = get_transcripts()
+      return paragraphs
     else:
       return "no file"
+
+
+@app.route('/analyze', methods = ["GET"])
+def analyze():
+  if request.method == "GET":
+    headers = {
+      "authorization": os.getenv("AAI_API_KEY"),
+    }
+    endpoint = db['transcription_url']
+    response = requests.get(endpoint, headers=headers)
+    print(response.json())
+    return response.json()
 
 
 app.run(host='0.0.0.0', port=8080)
